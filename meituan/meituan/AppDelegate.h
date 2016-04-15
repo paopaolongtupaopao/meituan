9045dee906b1280de442bc1b90be53638b02bfad
baoye <946761481@qq.com>
2016-04-15 09:46:13 +0800

    创建了meituan项目

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by 也鲍 on 16/4/15.
+//  Copyright © 2016年 by. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ meituan/meituan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  meituan
+//
+//  Created by 也鲍 on 16/4/15.
+//  Copyright © 2016年 by. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
